a93dae766426c0ab277136af1e06365c85842d09
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-05-06 19:08:52 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with the base URL
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://portpoliosid.onrender.com"
 });
 
 // Export the Axios instance
