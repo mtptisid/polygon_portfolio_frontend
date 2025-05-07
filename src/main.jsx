ba5254eee2ceb9aa7dddfa627084c29452adc5c8
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-05-08 03:59:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
+import ProjectsViewPage from './pages/ProjectsViewPage';
 import ErrorBoundary from './ErrorBoundary';
 import './index.css';
 
@@ -15,6 +16,7 @@ root.render(
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/projects-view" element={<ProjectsViewPage />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
