0b162b97f581027052124413d83f8a95bcacb90e
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-05-07 18:40:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
 import ErrorBoundary from './ErrorBoundary';
 import './index.css';
 
@@ -13,6 +14,7 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
