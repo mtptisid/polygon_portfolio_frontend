f74d33404b9db53c3e588833b65cf7d0fc3b04e7
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-05-18 11:06:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ProjectsPage from './pages/ProjectsPage';
 import ProjectsViewPage from './pages/ProjectsViewPage';
 import HomePage from './pages/HomePage'; 
 import ContactPage from './pages/ContactPage'
+import SendMailPage from './pages/SendMailPage';
 import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,6 +18,7 @@ root.render(
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/projects-view" element={<ProjectsViewPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/sendmail" element={<SendMailPage />} />
         <Route path="*" element={<div>Not Found</div>} /> 
       </Routes>
     </BrowserRouter>
