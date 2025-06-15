ae63d133dd7d43fad056685502a674ce7a96b1c5
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-06-15 14:53:14 +0530

    Update ProjectsViewPage.jsx

--- src/pages/ProjectsViewPage.jsx
+++ src/pages/ProjectsViewPage.jsx
@@ -158,6 +158,12 @@ const projects = [
         "technologies": ["Python", "Flask", "OpenCV", "Bootstrap"]
     },
     {
+    "title": "FLAN-T5 Small Fine-Tuning with LoRA",
+    "url": "https://github.com/mtptisid/FLAN-T5-Small_finetuning_LoRA",
+    "description": "A hands-on project to fine-tune FLAN-T5-Small using Low-Rank Adaptation (LoRA) for efficient NLP task adaptation. Demonstrates parameter-efficient fine-tuning (PEFT) techniques using Hugging Face and PEFT libraries. Great for quick experimentation on lower-end GPUs.",
+    "technologies": ["Python", "Hugging Face Transformers", "LoRA", "PEFT", "NLP", "FLAN-T5", "Machine Learning"]
+    },
+    {
         "title": "Personal_Voice_Assistant_with_OpenAI",
         "url": "https://github1s.com/mtptisid/Personal_Voice_Assistant_with_OpenAI",
         "description": "A personal voice assistant application that leverages OpenAI's API to process voice commands and provide intelligent responses. The assistant can perform tasks such as fetching information, setting reminders, and more, demonstrating the integration of speech recognition with AI-powered text generation.",
