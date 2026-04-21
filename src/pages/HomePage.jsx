88bbfe944d62f0f3367c9df706dd5880fcc09be3
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2026-04-21 12:24:36 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -312,8 +312,8 @@ const HomePage = () => {
       window.location.href = 'https://portfolio.siddharamayya.in';
     } else if (prompt === "Download Resume") {
       const link = document.createElement('a');
-      link.href = 'https://portfolio.siddharamayya.in/images/Mathapati_Siddharamayya_2025.pdf';
-      link.download = 'Mathapati_Siddharamayya_2025.pdf';
+      link.href = 'https://portfolio.siddharamayya.in/images/Mathapati_Siddharamayya.pdf';
+      link.download = 'Mathapati_Siddharamayya.pdf';
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
